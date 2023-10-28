62385a0985f5b7ee520d3335729c78fd1a13b09b
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2023-10-28 10:56:03 -0700

    using delta instead in heart cube

--- cubes/heart_cube.gd
+++ cubes/heart_cube.gd
@@ -1,7 +1,7 @@
 extends BaseCube
 
 var heart_cube_res = preload("res://cubes/heart_cube.tscn")
-var start_time = 0
+var time_alive = 0.0
 var time_to_grow = 5
 var spawned_child = false
 
@@ -9,14 +9,12 @@ func _init():
 	kind = "HEART"
 
 func _ready():
-	start_time = Time.get_ticks_msec()
 	label.text = "H%s" % time_to_grow
 
-func _process(delta):
-	#TODO use delta
-	var seconds_elapsed = floor((Time.get_ticks_msec() - start_time)/1000)
-	label.text = "H%s" % max(0, (time_to_grow - seconds_elapsed))
-	if not spawned_child and seconds_elapsed >= time_to_grow:
+func _process(delta): #delta: Seconds
+	time_alive += delta
+	label.text = "H%.1f" % max(0, (time_to_grow - time_alive))
+	if not spawned_child and time_alive >= time_to_grow:
 		var growth = heart_cube_res.instantiate()
 		growth.position = position + Vector3(0, 1, 0)
 		add_child(growth)
