445b2c52d944872328ffe66dad66a2962cffd5fa
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2023-10-28 15:52:23 -0700

    WIP: added wireframe scene and shader, heart cube uses it, needs color and filling up visuals

--- cubes/heart_cube.gd
+++ cubes/heart_cube.gd
@@ -1,21 +1,18 @@
 extends Node3D
 
 var heart_cube_res = preload("res://cubes/heart_cube.tscn")
-var time_alive = 0.0
+var wireframe_res = preload("res://cubes/wireframe.tscn")
 var time_to_grow = 5
 var spawned_child = false
 var kind = "HEART"
 
 @onready var label = $Label3D
 
-func _ready():
-	label.text = "H%s" % time_to_grow
-
 func _process(delta):
-	time_alive += delta
-	label.text = "H%.1f" % max(0, (time_to_grow - time_alive))
-	if not spawned_child and time_alive >= time_to_grow:
-		var growth = heart_cube_res.instantiate()
+	if not spawned_child:
+		var growth = wireframe_res.instantiate()
 		growth.position = position + Vector3(0, 1, 0)
+		growth.resource = heart_cube_res
+		growth.time_to_grow = time_to_grow
 		add_child(growth)
 		spawned_child = true
---
+++ cubes/wireframe.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+var time_alive = 0.0
+var time_to_grow
+var resource
+var realized = false
+
+@onready var wireframe_mesh = $MeshInstance3D
+
+func _process(delta):
+	time_alive += delta
+	if not realized and time_alive >= time_to_grow:
+		var real = resource.instantiate()
+		real.position = position + Vector3(0, -1, 0)
+		remove_child(wireframe_mesh)
+		add_child(real)
+		realized = true
